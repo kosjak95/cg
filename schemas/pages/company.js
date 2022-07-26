58367b29e0150990feecb288de813e54444c9b62
Jakub Kosman <jakub.kosman@fortedigital.no>
2022-07-26 15:43:01 +0200

    .

--- schemas/pages/company.js
+++ schemas/pages/company.js
@@ -13,5 +13,58 @@ export const Company = {
       type: "text",
       title: "Company Description",
     },
+    {
+      name: "employees",
+      type: "array",
+      title: "Employees",
+      of: [{ type: "string" }],
+      options: {
+        layout: "tags",
+      },
+    },
+    {
+      name: "employees2",
+      type: "array",
+      title: "Employees",
+      of: [
+        {
+          name: "emp",
+          type: "object",
+          title: "Employee",
+          fields: [
+            {
+              name: "firstName",
+              type: "string",
+            },
+            {
+              name: "lastName",
+              type: "string",
+            },
+            {
+              name: "address",
+              type: "text",
+            },
+            {
+              name: "salary",
+              type: "number",
+            },
+          ],
+        },
+      ],
+    },
+    // {
+    //   name: "employees3",
+    //   type: "array",
+    //   title: "Employees",
+    //   of: [
+    //     {
+    //       type: "reference",
+    //       to: [{ type: "employee" }],
+    //     },
+    //   ],
+    //   options: {
+    //     layout: "tags",
+    //   },
+    // },
   ],
 };
---
+++ schemas/pages/employee.js
@@ -0,0 +1,17 @@
+export const Employee = {
+  name: "employee",
+  type: "document",
+  title: "Employee",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name",
+    },
+    {
+      name: "address",
+      type: "text",
+      title: "Address",
+    },
+  ],
+};
