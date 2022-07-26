f4fa78894bf9e606f8ee12c0f97b216c5b5a9a43
Jakub Kosman <jakub.kosman@fortedigital.no>
2022-07-26 15:17:32 +0200

    .

---
+++ schemas/pages/company.js
@@ -0,0 +1,17 @@
+export const Company = {
+  name: "company",
+  type: "document",
+  title: "Company",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Company Name",
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Company Description",
+    },
+  ],
+};
